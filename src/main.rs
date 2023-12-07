82f2e9ae5153876cd3a4553bf56dd5679624b4c9
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-12-07 22:58:31 +0000

    Create a connection pool

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use sqlx::SqlitePool;
+use anyhow::Result;
+use std::env;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<()> {
+    // Create a connection pool for our database
+    let pool = SqlitePool::connect(&env::var("DATABASE_URL")?).await?;
+
+    println!("Initialised pool");
+
+    Ok(())
 }
