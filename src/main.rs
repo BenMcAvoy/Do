fec9676412e74b01d6d3a763f2390b243da146cd
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-12-08 12:10:59 +0000

    Print out errors with extra formatting

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use colored::Colorize;
 use do_cli::Do;
 
 use clap::Parser;
@@ -10,9 +11,13 @@ async fn main() -> Result<()> {
     let do_cli = Do::new().await?;
     let cli = DoCli::parse();
 
-    match cli.command {
-        Commands::List { name, hash } => do_cli.list_todos(name, hash).await?,
+    let result = match cli.command {
+        Commands::List { name, hash } => do_cli.list_todos(name, hash).await,
     };
+
+    if let Err(e) = result {
+        println!("{} {}", "[ ERROR ]".red().bold(), e.to_string().red());
+    }
 
     Ok(())
 }
