993451d84d2d8bc5cea5742cacce3f1718265438
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-12-08 15:56:01 +0000

    Store userID in the Do struct

--- src/lib.rs
+++ src/lib.rs
@@ -5,72 +5,80 @@ use colored::Colorize;
 use sqlx::{query, MySqlPool};
 use std::env;
 
+use crate::errors::DoError;
+
 pub mod commands;
+pub mod errors;
 
 pub struct Do {
     pool: MySqlPool,
+
+    userID: i32,
 }
 
 impl Do {
-    pub async fn new() -> Result<Self> {
+    pub async fn new(name: &str, hash: &str) -> Result<Self> {
         let pool = MySqlPool::connect(&env::var("DATABASE_URL")?).await?;
 
-        Ok(Self { pool })
-    }
+        let userID;
 
-    pub async fn list_todos(&self, user: String, hash: String) -> Result<()> {
         let users = query!(
             r#"
 SELECT UserID, Name, Hash
 FROM users
 WHERE users.Name = ? AND users.Hash = ?
         "#,
-            user,
+            name,
             hash
         )
-        .fetch_all(&self.pool)
+        .fetch_all(&pool)
         .await?;
 
-        let mut complete_tasks: Vec<String> = Vec::new();
-        let mut incomplete_tasks: Vec<String> = Vec::new();
 
         if let Some(user) = users.get(0) {
-            let tasks = query!(
-                r#"
+            userID = user.UserID;
+        } else {
+            return Err(DoError::InvalidLogin.into());
+        }
+
+        Ok(Self { pool, userID })
+    }
+
+    pub async fn list_todos(&self) -> Result<()> {
+        let tasks = query!(
+            r#"
 SELECT tasks.TaskID, tasks.Name, tasks.Description, tasks.Completed
 FROM todos
 JOIN users ON todos.UserID = users.UserID
 JOIN tasks ON todos.TaskID = tasks.TaskID
 WHERE users.UserID = ?;
                 "#,
-                user.UserID,
-            )
+                self.userID,
+                )
             .fetch_all(&self.pool)
             .await?;
 
-            for task in tasks {
-                let taskText = format!("{}: {}", task.Name, &task.Description);
+        let mut complete_tasks = Vec::new();
+        let mut incomplete_tasks = Vec::new();
 
-                match task.Completed != 0 {
-                    true => complete_tasks.push(taskText),
-                    false => incomplete_tasks.push(taskText),
-                }
+        for task in tasks {
+            match task.Completed != 0 {
+                true => complete_tasks.push(task),
+                false => incomplete_tasks.push(task),
             }
-        } else {
-            return Ok(());
         }
 
         if !complete_tasks.is_empty() {
             println!("{}", "Complete tasks:".green().bold());
             for task in complete_tasks {
-                println!("  {}", task.green());
+                println!("  {} - {}", task.Name.green(), task.Description.green());
             }
         }
 
         if !incomplete_tasks.is_empty() {
             println!("{}", "Incomplete tasks:".red().bold());
             for task in incomplete_tasks {
-                println!("  {}", task.red());
+                println!("  {} - {}", task.Name.red(), task.Description.red());
             }
         }
 
