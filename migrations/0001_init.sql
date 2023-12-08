641337a7dfaeb67d503ddf19d2a1e3da21beed06
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-12-08 02:41:42 +0000

    Update migration

--- migrations/0001_init.sql
+++ migrations/0001_init.sql
@@ -1,6 +1,21 @@
-CREATE TABLE IF NOT EXISTS todos
-(
-    id          INTEGER PRIMARY KEY NOT NULL,
-    description TEXT                NOT NULL,
-    done        BOOLEAN             NOT NULL DEFAULT 0
+CREATE TABLE `Tasks` (
+    `TaskId` INT  NOT NULL,
+    `Name` VARCHAR(255)  NOT NULL,
+    `Description` VARCHAR(255)  NOT NULL,
+    `UserID` INT  NOT NULL,
+    PRIMARY KEY (
+        `TaskId`
+    )
 );
+
+CREATE TABLE `Users` (
+    `UserId` INT  NOT NULL,
+    `Username` VARCHAR(255)  NOT NULL,
+    `Hash` VARCHAR(255)  NOT NULL,
+    PRIMARY KEY (
+        `UserId`
+    )
+);
+
+ALTER TABLE `Tasks` ADD CONSTRAINT `fk_Tasks_UserID` FOREIGN KEY(`UserID`)
+REFERENCES `Users` (`UserId`);
